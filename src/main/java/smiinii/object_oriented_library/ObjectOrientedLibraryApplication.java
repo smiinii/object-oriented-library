e132da7fe58478a37aee4f573e2e66eebcd0e887
smini <sm020320@naver.com>
2025-11-05 22:19:38 +0900

    chore(docker): 로컬 개발 환경에서 MySQL 컨테이너 연동 및 서버 연결 테스트

--- src/main/java/smiinii/object_oriented_library/ObjectOrientedLibraryApplication.java
+++ src/main/java/smiinii/object_oriented_library/ObjectOrientedLibraryApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ObjectOrientedLibraryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ObjectOrientedLibraryApplication.class, args);
 	}
--- src/main/java/smiinii/object_oriented_library/TestController.java
+++ src/main/java/smiinii/object_oriented_library/TestController.java
@@ -1,4 +1,12 @@
 package smiinii.object_oriented_library;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
 }
