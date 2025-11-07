ae7c3eb9c2611670b679274168034138895ed0c6
smini <sm020320@naver.com>
2025-11-07 16:48:26 +0900

    test(storedbook): 도서 대출 시 상태 전이 검증 테스트 추가

---
+++ src/test/java/smiinii/object_oriented_library/StoredBookTest.java
@@ -0,0 +1,42 @@
+package smiinii.object_oriented_library;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import smiinii.object_oriented_library.domain.StoredBook;
+import smiinii.object_oriented_library.domain.StoredBookStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StoredBookTest {
+
+    @Test
+    @DisplayName("AVAILABLE이면 대출 성공")
+    void loanSuccessTest() {
+        // given
+        StoredBook book = StoredBook.createAvailable(1L);
+        // when
+        book.loan(1L);
+        // then
+        assertThat(book.getStatus()).isEqualTo(StoredBookStatus.LOANED);
+    }
+
+    @Test
+    @DisplayName("이미 대출 중이면 예외처리")
+    void loanFailedWhenAlreadyLoaned() {
+        // given
+        StoredBook book = StoredBook.createAvailable(1L);
+        book.loan(1L);
+        // when & then
+        assertThatThrownBy(() -> book.loan(1L)).isInstanceOf(IllegalStateException.class);
+    }
+    
+    @Test
+    @DisplayName("다른 회원이 예약 중이면 예외처리")
+    void loanFailedWhenOnHold() {
+        // given
+        StoredBook book = StoredBook.createOnHold(1L);
+        // when & then
+        assertThatThrownBy(() -> book.loan(1L)).isInstanceOf(IllegalStateException.class);
+    }
+}
