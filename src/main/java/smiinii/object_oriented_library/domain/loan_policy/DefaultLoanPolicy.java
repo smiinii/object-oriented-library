9bcf2f2915d3cee55691d07316210aec142da42c
smini <sm020320@naver.com>
2025-11-15 20:05:12 +0900

    feat: 기본 7일 대출/연장 규칙을 적용한 DefaultLoanPolicy 구현

---
+++ src/main/java/smiinii/object_oriented_library/domain/loan_policy/DefaultLoanPolicy.java
@@ -0,0 +1,16 @@
+package smiinii.object_oriented_library.domain.loan_policy;
+
+import java.time.LocalDateTime;
+
+public class DefaultLoanPolicy implements LoanPolicy {
+
+    @Override
+    public LocalDateTime initialDueDate(LocalDateTime loanedAt) {
+        return loanedAt.plusDays(7);
+    }
+
+    @Override
+    public LocalDateTime extendedDueDate(LocalDateTime dueDate) {
+        return dueDate.plusDays(7);
+    }
+}
