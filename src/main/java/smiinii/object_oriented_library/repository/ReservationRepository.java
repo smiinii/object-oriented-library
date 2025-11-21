ca0a208bfb18b2732603e324ebfc0717efc01b57
smini <sm020320@naver.com>
2025-11-21 13:22:54 +0900

    refactor: 필요없는 Repository 제거

--- src/main/java/smiinii/object_oriented_library/repository/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package smiinii.object_oriented_library.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import smiinii.object_oriented_library.domain.reservation.Reservation;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
